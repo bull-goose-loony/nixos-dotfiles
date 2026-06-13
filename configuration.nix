9cdd9ae548324d5b94d5bede728af3671511355a
zach <zagrdev@gmail.com>
2026-06-13 12:12:50 -0600

    cleaned up and did some renaming

--- configuration.nix
+++ configuration.nix
@@ -1,49 +1,49 @@
 {
   config,
-  lib,
-  pkgs,
-  ...
+    lib,
+    pkgs,
+    ...
 }: {
   imports = [
     ./hardware-configuration.nix
-    ./niri-setup.nix
-    ./laptop.nix
+      ./niri-setup.nix
+      ./laptop.nix
   ];
 
-  nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 
   boot.loader.timeout = 300;
   boot.loader.grub = {
     enable = true;
     efiSupport = true;
     device = "nodev"; # REQUIRED for UEFI
-    useOSProber = true; # Detect Linux Mint
-    configurationLimit = 5;
+      useOSProber = true; # Detect Linux Mint
+      configurationLimit = 5;
   };
-
   boot.loader.efi = {
     canTouchEfiVariables = true;
     efiSysMountPoint = "/boot/efi";
   };
 
-  # Network
-  networking.hostName = "my-nixos";
+# This hostname can be whatever, doesn't need to match the Nix Flake host 
+  networking.hostName = "zachs-nixos";
   networking.networkmanager.enable = true;
-
-  # Set your time zone.
   time.timeZone = "America/Denver";
-
-  services.getty.autologinUser = "zach";
-
-  programs.zsh.enable = true;
 
   virtualisation.docker = {
     enable = true;
     enableOnBoot = true;
   };
 
-  # This allows me to SSH into this machine from any machine
-  # that has the public key defined in users.users.zach
+# Have nix auto-collect old garbage
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
+# This allows me to SSH into this machine from any machine
+# that has the public key defined in users.users.zach
   services.openssh = {
     enable = true;
     settings = {
@@ -53,40 +53,31 @@
     };
     openFirewall = true;
   };
+  services.getty.autologinUser = "zach";
 
-  #  Define a user account. Don't forget to set a password with ‘passwd’.
+#  Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.zach = {
     shell = pkgs.zsh;
     isNormalUser = true;
     extraGroups = ["wheel" "networkmanager" "docker"]; # Enable ‘sudo’ for the user.
-    openssh.authorizedKeys.keys = [
-      #ssh-ed25519 <some-public-key> zach@thinkpad
-    ];
-    packages = with pkgs; [
-      tree
-    ];
+      openssh.authorizedKeys.keys = [
+#ssh-ed25519 <some-public-key> zach@thinkpad
+      ];
   };
 
+  programs.zsh.enable = true;
   programs.firefox.enable = true;
 
-  # Have nix auto-collect old garbage
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 30d";
-  };
-
+  nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
     brightnessctl
-    vim
-    wget
-    git
-    pulseaudio
-    parted
-    efibootmgr
+      vim
+      wget
+      git
+      pulseaudio
+      parted
+      efibootmgr
   ];
-
-  nix.settings.experimental-features = ["nix-command" "flakes"];
 
   system.stateVersion = "25.11"; # Did you read the comment?
 }
--- flake.nix
+++ flake.nix
@@ -1,28 +1,52 @@
+# Without a flake, we have no control over where nixpkgs comes from.
+# and it could also change from build to build. The old way of configuring
+# a NixOS system (configuration.nix at the top level) gave the user access to
+# a nixpkgs through a `nix-channel`. The problem was that nix-channel could 
+# give you a different version of nixpkgs than the previous build with no
+# way to control package versions you were getting from it.
+
+# Enter flake.nix. This file let's you choose exactly where your nixpkgs
+# come from *down to the commit*. But that's not all. It lets you define
+# any dependency you would like to have under this flake. 
+
+# In short, a flake is a hyper-repeatable build, this is perfect for
+# dev environments. If ACES microservices couldn't control their jaxson 
+# version for example, they would break constantly so it's essentially
+# developer-level control over the nature of your system.
+
+# You don't have to get rid of your configuration.nix if your going from
+# old style to new style. All you gotta do is import it as a module in
+# the outputs set.
+
+# I like to think of flakes as a sort of Dockerfile, we can build this
+# system with this version of gcc and this old version of valgrend etc.
+
 {
-  description = "Hyprland on Nixos";
+  description = "Zach's first NixOS flake.nix";
 
   inputs = {
-
     nixpkgs.url = "nixpkgs/nixos-unstable";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs"; # this means that home-manager will use the nixpkgs that this system is using
-    };
+    myHomeManager.url = "github:nix-community/home-manager";
+    myHomeManager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
+
+  # This syntax is equivalent to
+  #
+  # NixOSSystem outputs(NixPkgs nixpkgs, HomeManager homeManager) {}
+  #  
   outputs = {
     nixpkgs,
-    home-manager,
+    myHomeManager,
     ...
   }: {
     # given the inputs (nixpkgs & home-manager), output a host
     # nixpkgs.lib.nixosSystem builds it 
-    nixosConfigurations.my-nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.zachsNixosFlake = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
-        home-manager.nixosModules.home-manager
+        myHomeManager.nixosModules.home-manager
         {
           home-manager = {
             useGlobalPkgs = true;
